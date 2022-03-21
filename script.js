db8f24c6efdb2471a564e2013b4dfabe5938fce7
Hilmi Eralp <hilmiozdurdu@hotmail.com>
2022-03-21 20:55:59 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);    
+    document.getElementById("result").innerHTML="If you deposit "+"<span class = 'hightext'>"+principal+"</span>"+",\<br\>at an interest rate of "+"<span class = 'hightext'>"+rate+"</span>"+"%\<br\>You will receive an amount of "+"<span class = 'hightext'>"+interest+"</span>"+",\<br\>in the year "+"<span class = 'hightext'>"+year+"</span>"+"\<br\>"
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
